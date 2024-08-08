b5de56c4fef76c13aee09374b64b2eac09d3f6d5
Shanmukh459 <saikrrish459@gmail.com>
2024-08-08 13:20:19 -0700

    working with context

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
+import { useReducer } from "react"
 import "./App.css"
 import LoginStatus from "./state-management/LoginStatus"
+import TaskList from "./state-management/TaskList"
+import tasksReducer from "./state-management/reducers/tasksReducer"
+import TasksContext from "./state-management/contexts/TasksContext"
+import NavBar from "./state-management/NavBar"
+import HomePage from "./state-management/HomePage"
 
 function App() {
-  return <LoginStatus />
+  const [tasks, dispatch] = useReducer(tasksReducer, [])
+
+  return (
+    <TasksContext.Provider value={{ tasks, dispatch }}>
+      <NavBar />
+      <HomePage />
+    </TasksContext.Provider>
+  )
 }
 
 export default App
