d56cecb91614bd8296eacd883a28e31ecaa1aff3
Shanmukh459 <saikrrish459@gmail.com>
2024-08-09 12:33:16 -0700

    organizing counter and auth

--- src/state-management/tasks/TaskList.tsx
+++ src/state-management/tasks/TaskList.tsx
@@ -1,6 +1,6 @@
 import { useContext } from "react"
-import useAuth from "../hooks/useAuth"
 import TasksContext from "./tasksContext"
+import useAuth from "../auth/useAuth"
 
 const useTasks = () => useContext(TasksContext)
 
