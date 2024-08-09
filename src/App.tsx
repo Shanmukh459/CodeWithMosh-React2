f4a7a2eabc7f59d6a1b7c304b245472626a8bee2
Shanmukh459 <saikrrish459@gmail.com>
2024-08-09 12:14:23 -0700

    organizing code for scalability and maintainability

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
-import { useReducer } from "react"
 import "./App.css"
-import tasksReducer from "./state-management/reducers/tasksReducer"
 import NavBar from "./state-management/NavBar"
 import HomePage from "./state-management/HomePage"
 import AuthProvider from "./state-management/AuthProvider"
-import TasksProvider from "./state-management/TasksProvider"
+import { TasksProvider } from "./state-management/tasks"
 
 function App() {
   return (
